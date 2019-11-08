02513a6972a808b520e04b728fdbc5761658b2d3
cdub520 <37935484+cdub520@users.noreply.github.com>
2019-11-08 16:54:41 -0500

    added lots o features

--- js_bin/map_generator.js
+++ js_bin/map_generator.js
@@ -1,14 +1,16 @@
+
 function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
             center: {
                   lat: -34.397,
                   lng: 150.644
             },
-            zoom: 8
+            zoom: 15
       });
 }
-
+lineArray=[];
 function generatePath(debug = false) {
+      console.log("GeneratePath\nDebug:\t"+debug+"\nSearch Criteria:\t"+document.getElementById("search_field").value);
       if (debug == false) {
             pathPoints = [];
             search = document.getElementById("search_field").value;
@@ -18,14 +20,16 @@ function generatePath(debug = false) {
                         jsonObj = JSON.parse(this.responseText);
                         for (i in jsonObj) {
                               pathPoints.push({
-                                    'lat': jsonObj[i].lat,
-                                    'lng': jsonObj[i].lon
+                                    'lat': jsonObj[i].latitude,
+                                    'lng': jsonObj[i].longitude
                               });
+                              document.getElementById("results_table_body").innerHTML+="<tr><td>"+i+'</td>'+'<td> FEATURE NOT AVAILABLE</td><td>'+jsonObj[i].latitude+'</td><td>'+jsonObj[i].longitude+'</td><td>FEATURE NOT YET AVAILABLE</td></tr>'
                         }
+                        console.log(pathPoints);
                         populateMap(pathPoints);
                   }
             };
-            xhttp.open("POST", "http://54.175.22.220/cgi-bin/queryPage.py", true);
+            xhttp.open("POST", "http://54.175.22.220/cgi-bin/querypage.py", true);
             xhttp.send("tag=" + search);
       } else {
             var pathPoints = [{
@@ -59,4 +63,25 @@ function populateMap(pathPoints) {
       });
 
       newpath.setMap(map);
+      lineArray.push(newpath);
+}
+function getCenterOfMap(tag){
+      console.log("getCenterOfMap\ntag:\t"+tag);
+     var xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+               jsonObj=JSON.parse(this.responseText);
+               console.log(jsonObj.latitude,jsonObj.longitude)
+               map.setCenter(new google.maps.LatLng(jsonObj.latitude,jsonObj.longitude));
+          }
+     };
+     xhttp.open("POST", "http://54.175.22.220/cgi-bin/getIntLocation.py", true);
+     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+     xhttp.send("tag="+tag);
+}
+function clearPaths(){
+      for (i=0; i<lineArray.length;i++){
+            lineArray[i].setMap(null);
+      }
 }
